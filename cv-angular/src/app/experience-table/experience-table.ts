90bd72bf7ce1c3e7cc2e45ee033e352c275865f3
Antonio-3108 <142509130+Antonio-3108@users.noreply.github.com>
2025-10-01 16:05:11 -0600

    Revert "test"
    This reverts commit 6c6fb61287a1bd3458f03a80c7f78dfd8215002a.

---
+++ cv-angular/src/app/experience-table/experience-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-table',
+  imports: [],
+  templateUrl: './experience-table.html',
+  styleUrl: './experience-table.css'
+})
+export class ExperienceTable {
+
+}
